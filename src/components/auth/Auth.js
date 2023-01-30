987f85b96a824ca7953de19b468fed6fff2f0e1e
Grey Altr <grey.sifr@gmail.com>
2023-01-30 15:44:29 -0800

    added auth-box html to Auth.js component

--- src/components/auth/Auth.js
+++ src/components/auth/Auth.js
@@ -1,9 +1,51 @@
 import React from 'react';
 import { useState } from 'react';
+import { NavLink, useParams } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
 
 export default function Auth() {
   const [email, setEmail] = useState('');
   const [password, setPass] = useState('');
+  const { type } = useParams();
+  const { user, setUser } = useUser();
 
-  return <div>Welcome to your To-do List Manager</div>;
+  return (
+    <div className="auth box">
+      <div className="panel-heading">Welcome to your To-do List Manager</div>
+      <div className="panel-tabs">
+        <NavLink to="/auth/sign-in" activeClassName="is-active">
+          Sign In
+        </NavLink>
+        <NavLink to="auth/sign-up" activeClassName="is-active">
+          Sign Up
+        </NavLink>
+      </div>
+      <div className="panel-block">
+        <div className="field">
+          <label>Email</label>
+          <div>
+            <input
+              className="input"
+              type="email"
+              placeholder="name@example.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+        </div>
+        <div className="field">
+          <label className="label">Password</label>
+          <div>
+            <input
+              className="input"
+              type="password"
+              placeholder="password"
+              value={password}
+              onChange={(e) => setPass(e.target.value)}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
