7dbaacb0cf49decf3ab8620d15eeb4c5f8e9a0f1
Grey Altr <grey.sifr@gmail.com>
2023-01-30 17:58:48 -0800

    ItemsList.js started

--- src/components/items/Items.js
+++ src/components/items/Items.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Redirect } from 'react-router-dom';
+import { useUser } from '../../context/UserContext.js';
 
 export default function Items() {
-  return <div>Items</div>;
+  const { user } = useUser();
+
+  if (!user) {
+    return <Redirect to="/auth/sign-in" />;
+  }
+  return (
+    <div>
+      <div>ItemsList ItemsForm</div>
+    </div>
+  );
 }
--- src/services/items.js
+++ src/services/items.js
@@ -1,12 +1,12 @@
 import { checkError, client } from './client.js';
 
 export async function getListItems() {
-  const response = await client.from('tasks').select();
+  const response = await client.from('todos').select();
   return checkError(response);
 }
 
 export async function createListItem(id, description) {
-  const response = await client.from('tasks').insert([{ id, description }]).single();
+  const response = await client.from('todos').insert([{ id, description }]).single();
 
   return checkError(response);
 }
